e6c4957fa28299abc8b5d27c62471f4b2f8327fc
ren <ren@ren-pc.localdomain>
2019-08-19 06:21:50 -0700

    Add calendar_time.c

--- time/calendar_time.c
+++ time/calendar_time.c
@@ -9,7 +9,8 @@ int main()
 {
     time_t t;
     struct timeval tv;
-
+    struct tm *gmp, *locp;
+    struct tm gm, loc;
 
     t = time(NULL);
     printf("Seconds since Epoch(1.1.1970): %ld", (long)t);
@@ -19,5 +20,34 @@ int main()
         errExit("gettimeofday");
     printf("gettimeofday() returned %ld secs, %ld microsecs\n",
                 (long)tv.tv_sec, (long)tv.tv_usec);
+    
+    gmp = gmtime(&t);
+    if (gmp == NULL)
+        errExit("gmtime");
+    
+    gm = *gmp;
+
+    printf("Break down by gmtime():\n");
+    printf("    year=%d mon=%d mday=%d hour=%d min=%d sec=%d ",
+                gm.tm_year, gm.tm_mon, gm.tm_mday, gm.tm_hour, gm.tm_min, gm.tm_sec);
+    printf("wday=%d yday=%d isdst=%d\n", gm.tm_wday, gm.tm_yday, gm.tm_isdst);
+    
+    locp = localtime(&t);
+    if (locp == NULL)
+        errExit("localtime");
+    
+    loc = *locp;
+
+    printf("Break down by localtime():\n");
+    printf("    year=%d mon=%d mday=%d hour=%d min=%d sec=%d ",
+                loc.tm_year, loc.tm_mon, loc.tm_mday, loc.tm_hour, loc.tm_min, loc.tm_sec);
+    printf("wday=%d yday=%d isdst=%d\n\n", loc.tm_wday, loc.tm_yday, loc.tm_isdst);
+
+    printf("asctime() formats the gmtime() value as: %s", asctime(&gm));
+    printf("ctime() formats the time() value as: %s", ctime(&t));
+
+    printf("mktime() of gmtime() value:    %ld secs\n", (long)mktime(&gm));
+    printf("mktime() of localtime() value: %ld secs\n", (long)mktime(&loc));
+
     return 0;
 }
